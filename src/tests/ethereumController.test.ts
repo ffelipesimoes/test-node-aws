d438eee4105a76fd79f46557b7753531eebbe5da
Felipe Simões <ffelipesimoes@gmail.com>
2023-09-15 07:38:47 -0300

    refact: tests fixed

--- src/tests/ethereumController.test.ts
+++ src/tests/ethereumController.test.ts
@@ -1,7 +1,7 @@
-import { generateEthereumKeys } from '../utils/ethereumUtils';
+import { generateEthereumKeys, getEthereumAddressFromPublicKey } from '../utils/ethereumUtils';
 import * as ethUtil from 'ethereumjs-util';
 
-describe('Ethereum Utils - Key Generation', () => {
+describe('Key Generation', () => {
   it('should generate private and public keys', () => {
     const keys = generateEthereumKeys();
     expect(keys.privateKey).toBeDefined();
@@ -16,7 +16,23 @@ describe('Ethereum Utils - Key Generation', () => {
 
   it('should derive the public key from the private key', () => {
     const keys = generateEthereumKeys();
-    const derivedPublicKey = ethUtil.privateToPublic(Buffer.from(keys.privateKey, 'hex')).toString('hex');
+    let derivedPublicKey = ethUtil.privateToPublic(Buffer.from(keys.privateKey, 'hex')).toString('hex');
+  
+    if (!derivedPublicKey.startsWith('04')) {
+      derivedPublicKey = '04' + derivedPublicKey;
+    }
+  
     expect(derivedPublicKey).toBe(keys.publicKey);
+  });
+  
+});
+
+describe('Address Generation', () => {
+  it('should generate a valid Ethereum address from a public key', () => {
+    const keys = generateEthereumKeys();
+    const address = getEthereumAddressFromPublicKey(keys.publicKey);
+    expect(address).toBeDefined();
+    expect(address.length).toBe(42); 
+    expect(address.startsWith('0x')).toBe(true);
   });
 });
--- src/utils/ethereumUtils.ts
+++ src/utils/ethereumUtils.ts
@@ -4,10 +4,21 @@ import * as crypto from 'crypto';
 export const generateEthereumKeys = () => {
   const privateKeyBuffer = crypto.randomBytes(32);
   const privateKey = privateKeyBuffer.toString('hex');
-  const publicKey = ethUtil.privateToPublic(privateKeyBuffer).toString('hex');
+  let publicKey = ethUtil.privateToPublic(privateKeyBuffer).toString('hex');
+
+  if (!publicKey.startsWith('04')) {
+    publicKey = '04' + publicKey;
+  }
 
   return {
     privateKey,
     publicKey
   };
+};
+
+
+export const getEthereumAddressFromPublicKey = (publicKey: string) => {
+  const publicKeyBuffer = Buffer.from(publicKey, 'hex');
+  const addressBuffer = ethUtil.publicToAddress(publicKeyBuffer, true);
+  return '0x' + addressBuffer.toString('hex');
 };
