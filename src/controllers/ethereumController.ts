117674faf3d5312878068a29649da6104e95e412
Felipe Simões <ffelipesimoes@gmail.com>
2023-09-15 07:29:06 -0300

    feat: get eth eoa

--- src/controllers/ethereumController.ts
+++ src/controllers/ethereumController.ts
@@ -8,4 +8,10 @@ export const generateKeys = (req: Request, res: Response) => {
   res.json(keys);
 };
 
-
+export const getEthereumAddress = (req: Request, res: Response) => {
+  const { publicKey }: { publicKey: string } = req.body;
+  const publicKeyBuffer = Buffer.from(publicKey, 'hex'); 
+  const addressBuffer = ethUtil.publicToAddress(publicKeyBuffer, true);
+  const address = '0x' + addressBuffer.toString('hex'); 
+  res.json({ address });
+};
